6368e771f07224d7f302e8f064e3b186e771de85
Chengyi-Hua <96745479+Chengyi-Hua@users.noreply.github.com>
2025-10-01 11:34:20 +0200

    adding information and files for assignment 1

---
+++ assignment_1_Naive_Bayes/a01_6.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 6 Preprocessing Features and Continuous Naive Bayes (optional)
+
+# %%
+import math
+import numpy as np
+import sklearn, sklearn.metrics
+# %load_ext autoreload
+# %autoreload 2
+
+from a01_helper import *
+from a01_functions import logsumexp
+
+
+# %%
+# YOUR CODE HERE
